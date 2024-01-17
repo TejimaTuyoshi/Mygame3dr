656487a7b5a0e757905b8e0344f7c51ad174a070
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-01-17 10:01:32 +0900

    プレイヤーにクールタイムを設けるように修正

--- My project/Assets/Script/Player.cs
+++ My project/Assets/Script/Player.cs
@@ -10,7 +10,7 @@ public class Player : MonoBehaviour
     bool stop = false;
     Vector3 force = new Vector3(0.0f, 0.0f, 50.0f);    // 力を設定
     Vector3 back = new Vector3(0.0f, 0.0f, -50.0f);    // 力を設定
-    int cooltime = 0;
+    float cooltime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,29 +20,43 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("w"))
+        Debug.Log(cooltime);
+        if (Input.GetKeyDown("w")&& stop == false)
         {
             transform.position += transform.TransformDirection(Vector3.forward) * 270f * Time.deltaTime;
+            stop = true;
         }
-        if (Input.GetKeyDown("s"))
+        if (Input.GetKeyDown("s") && stop == false)
         {
             transform.position += transform.TransformDirection(Vector3.back) * 270f * Time.deltaTime;
+            stop = true;
         }
-        if (Input.GetKeyDown("a"))
+        if (Input.GetKeyDown("a") && stop == false)
         {
             Transform myTransform = this.transform;
             // ローカル座標を基準に、回転を取得
             Vector3 localAngle = myTransform.localEulerAngles;
             localAngle.y -= 90.0f; // ローカル座標を基準に、y軸を軸にした回転を10度に変更
             myTransform.localEulerAngles = localAngle; // 回転角度を設定
+            stop = true;
         }
-        if (Input.GetKeyDown("d"))
+        if (Input.GetKeyDown("d") && stop == false)
         {
             Transform myTransform = this.transform;
             // ローカル座標を基準に、回転を取得
             Vector3 localAngle = myTransform.localEulerAngles;
             localAngle.y += 90.0f; // ローカル座標を基準に、y軸を軸にした回転を10度に変更
             myTransform.localEulerAngles = localAngle; // 回転角度を設定
+            stop = true;
+        }
+        if (stop == true)
+        {
+            cooltime += Time.deltaTime;
+        }
+        if (cooltime <= 0.1)
+        {
+            cooltime = 0;
+            stop = false;
         }
     }
 
